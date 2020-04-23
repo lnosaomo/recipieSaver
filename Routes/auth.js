11d9c71e52a50b00d1175a4f6fa97eeac91eff2c
Love <lnosaomo@gmail.com>
2020-04-23 01:07:13 -0400

    add other routes

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const config = require("config");
+const auth = require("../middleware/auth")
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
@@ -11,8 +12,17 @@ const User = require("../models/User");
 //@desc    Get logged in user
 //@access  Private
 
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async(req, res) => {
+
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user);
+    console.log(req)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+
 });
 
 //@route   POST api/auth
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,24 @@
 const config = require("config");
 const jwt = require("jsonwebtoken");
+
+module.exports = function(req, res, next) {
+  //get token from the header
+
+  const token = req.header('x-auth-token');
+
+  //check if no token
+
+  if (!token) {
+    return res.status(401).json({ msg: "No Token, auth denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    req.user = decoded.user;
+
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
