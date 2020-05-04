92d3d9282e40868c68b7fa7429b2d87d24c90fbe
Love Nosa-Omorogiuwa <lovenosa-omorogiuwa@Loves-MacBook-Air.local>
2020-05-03 21:10:44 -0400

    change name

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client-view/build'));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client-view', 'build', 'index.html'))
   );
 }
 const PORT = process.env.PORT || 5002;
