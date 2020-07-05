cfe012c61a4f8a2629d0b2813df8e45fa5664b7e
Love Nosa-Omorogiuwa <lnosaomo@gmail.com>
2020-07-04 22:33:49 -0400

    commit suggestions changes

--- client-view/src/components/Recipie.js
+++ client-view/src/components/Recipie.js
@@ -1,18 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment, useRef } from 'react';
 import axios from 'axios';
 import RecipieItem from './layout/RecipieItem';
+import { v4 as uuidv4 } from 'uuid';
+import './layout/autoSuggest.css';
 import '../components/Recipie.css';
+
 const Recipie = () => {
   const [recipies, setRecipies] = useState([]);
   const [search, setSearch] = useState({
     mealName: '',
     type: 'personal'
   });
-  const code = [];
-  const { mealName, type } = search;
 
-  const onChange = e => {
-    setSearch({ ...search, [e.target.name]: e.target.value });
+  const [activeSuggestion, setactiveSuggestion] = useState(0);
+  const [ress, setRess] = useState([]);
+  const [foodName, setfoodName] = useState(' ');
+
+  const [filteredSuggestions, setfilteredSuggestions] = useState([]);
+  const [showSuggestions, setshowSuggestions] = useState(true);
+  const { mealName, type } = search;
+  const text = useRef('');
+
+  const onchangefoodName = e => {
+    setfoodName(e.target.value);
+    const foodName = e.target.value;
+    var cors_api_host = 'https://cors-anywhere.herokuapp.com/';
+    // e.preventDefault();
+    axios
+      .get(
+        `${cors_api_host}https://api.edamam.com/auto-complete?q=${foodName}&app_id=313605df&app_key=3a360d7219529db4accf27b5c25d9845`
+      )
+      .then(response => {
+        //setRecipies(response.data.hits);
+        setRess(response.data);
+      });
+    // Filter our suggestions that don't contain the user's input
+    const filteredSuggestions = ress.filter(
+      suggestion => suggestion.indexOf(foodName) > -1
+    );
+
+    //debugger;
+    console.log(ress);
+
+    // Update the user input and filtered suggestions, reset the active
+    // suggestion and make sure the suggestions are shown
+
+    setactiveSuggestion(0);
+    setfilteredSuggestions(filteredSuggestions);
+    setshowSuggestions(true);
+    setfoodName(e.currentTarget.value);
   };
 
   const onSubmit = e => {
@@ -21,50 +57,106 @@ const Recipie = () => {
     e.preventDefault();
     axios
       .get(
-        `${cors_api_host}https://api.edamam.com/search?q=${mealName}&app_id=313605df&app_key=3a360d7219529db4accf27b5c25d9845&from=0&to=3&calories=591-722&health=alcohol-free`
+        `${cors_api_host}https://api.edamam.com/search?q=${foodName}&app_id=313605df&app_key=3a360d7219529db4accf27b5c25d9845`
       )
       .then(res => {
         setRecipies(res.data.hits);
       });
-
     console.log(recipies);
   };
 
+  // Event fired when the user clicks on a suggestion
+  const onClick = e => {
+    // Update the user input and reset the rest of the state
+
+    setactiveSuggestion(0);
+    setfilteredSuggestions([]);
+    setshowSuggestions(false);
+    setfoodName(e.currentTarget.innerText);
+  };
+
+  // Event fired when the user presses a key down
+  const onKeyDown = e => {
+    // User pressed the enter key, update the input and close the
+    // suggestions
+    if (e.keyCode === 13) {
+      setactiveSuggestion(0);
+      setshowSuggestions(false);
+      setfoodName(filteredSuggestions[activeSuggestion]);
+    }
+    // User pressed the up arrow, decrement the index
+    else if (e.keyCode === 38) {
+      if (activeSuggestion === 0) {
+        return;
+      }
+
+      setactiveSuggestion(activeSuggestion - 1);
+    }
+    // User pressed the down arrow, increment the index
+    else if (e.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length) {
+        return;
+      }
+
+      setactiveSuggestion(activeSuggestion + 1);
+    }
+  };
+
+  let suggestionsListComponent;
+  if (showSuggestions) {
+    if (ress.length > 0) {
+      suggestionsListComponent = (
+        <ul class='suggestions'>
+          {ress.map((suggestion, index) => {
+            let className;
+
+            // Flag the active suggestion with a class
+            if (index === activeSuggestion) {
+              className = 'suggestion-active';
+            }
+
+            return (
+              <li className={className} key={suggestion} onClick={onClick}>
+                {suggestion}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      suggestionsListComponent = (
+        <div class='no-suggestions'>
+          <em>No suggestions, you're on your own!</em>
+        </div>
+      );
+    }
+  }
+
   return (
     <div>
-      <form className='navbar-form navbar-left'>
+      <form className='navbar-form navbar-left' onSubmit={onSubmit}>
         <label>Meal Name:</label>
-        <input
-          type='text'
-          name='mealName'
-          value={mealName}
-          onChange={onChange}
-          className='form-control col-lg-8=6'
-        />
-        <h6>Meal Type:</h6>
-        <input
-          type='radio'
-          name='type'
-          value='personal'
-          onChange={onChange}
-          checked={type === 'personal'}
-        />{' '}
-        Personal
-        {'    '}
-        <input
-          type='radio'
-          name='type'
-          value='professional'
-          onChange={onChange}
-          checked={type === 'professional'}
-        />{' '}
-        Professional
+        <Fragment>
+          <input
+            className='input'
+            type='text'
+            id='foodName'
+            ref={text}
+            placeholder='type in food name'
+            value={foodName}
+            onChange={onchangefoodName}
+            onKeyDown={onKeyDown}
+            autocomplete='off'
+          />
+
+          {suggestionsListComponent}
+        </Fragment>
+
         <div>
           <input
             type='submit'
             value='Get Recipies'
-            className='btn btn-primary'
-            onClick={onSubmit}
+            className='btn btn-primary get-recipies'
           />
         </div>
       </form>
@@ -72,7 +164,9 @@ const Recipie = () => {
       <div className='accordion' id='accordionExample'>
         <div>
           {recipies &&
-            recipies.map(recipie => <RecipieItem recipie={recipie} />)}
+            recipies.map(recipie => (
+              <RecipieItem recipie={recipie} key={uuidv4()} id={uuidv4()} />
+            ))}
         </div>
       </div>
     </div>
--- client-view/src/components/layout/RecipieItem.js
+++ client-view/src/components/layout/RecipieItem.js
@@ -1,34 +1,71 @@
 import React from 'react';
 
-const RecipieItem = ({ recipie }) => {
-  console.log(recipie);
+const RecipieItem = ({ recipie, id }) => {
+  const openRecipieSource = url => {
+    window.open(`'${url}'`, '_blank');
+  };
   return (
-    <div className='card'>
+    <div className='card container'>
       <div className='card-header' id='headingOne'>
         <h2 className='mb-0'>
+          <div></div>
+          <div className='text-right right'>
+            {' '}
+            <a href='#' className=' btn btn-link'>
+              Save recipe
+            </a>
+          </div>
           <button
-            className='btn btn-link'
+            className='btn btn-secondary'
             type='button'
             data-toggle='collapse'
-            data-target='#collapseOne'
+            data-target={`#collapseOne${id}`}
             aria-expanded='true'
-            aria-controls='collapseOne'
+            aria-controls={`collapseOne${id}`}
           >
             {recipie.recipe.label}
           </button>
         </h2>
+
+        <img
+          src={`${recipie.recipe.image}`}
+          data-toggle='collapse'
+          data-target={`#collapseOne${id}`}
+          aria-expanded='true'
+          aria-controls={`collapseOne${id}`}
+          class='img-thumbnail rounded float-left'
+        ></img>
       </div>
 
       <div
-        id='collapseOne'
+        id={`collapseOne${id}`}
         className='collapse show'
         aria-labelledby='headingOne'
         data-parent='#accordionExample'
       >
         <div className='card-body'>
-          {recipie.recipe.ingredientLines.map(line => (
-            <li>{line}</li>
-          ))}
+          <div className='text-right'>
+            {' '}
+            <p class='badge badge-primary'>
+              Total Calories: {recipie.recipe.calories}
+            </p>
+          </div>
+          <div>
+            {' '}
+            <strong>Ingredients</strong>
+            {recipie.recipe.ingredientLines.map(line => (
+              <li>{line}</li>
+            ))}
+          </div>
+          <br />
+          <button
+            className='btn btn-secondary'
+            type='button'
+            // onClick={openRecipieSource(recipie.recipe.url)}
+          >
+            Get Instructions
+          </button>
+          <strong>From: {recipie.recipe.source}</strong>
         </div>
       </div>
     </div>
