416cdb899265757dbd6622a7e715be6f9c69ded8
Love Nosa-Omorogiuwa <lnosaomo@gmail.com>
2020-07-16 12:34:17 -0400

    remove credentials

--- client-view/src/context/types.js
+++ client-view/src/context/types.js
@@ -22,3 +22,4 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const SUCCESS_STATUS = 'SUCCESS_STATUS';
